1e75d62dc48fcf92990b7e47eb97c8fbdfceaf4a
Keith Lazuka <kelazuka@microsoft.com>
2019-07-26 19:18:55 -0400

    checkpoint: converted ElmTestProgramRunner

--- src/main/java/org/frawa/elmtest/run/ElmTestProgramRunner.kt
+++ src/main/java/org/frawa/elmtest/run/ElmTestProgramRunner.kt
@@ -1,48 +1,34 @@
-package org.frawa.elmtest.run;
+package org.frawa.elmtest.run
 
-import com.intellij.execution.ExecutionException;
-import com.intellij.execution.ExecutionResult;
-import com.intellij.execution.configurations.RunProfile;
-import com.intellij.execution.configurations.RunProfileState;
-import com.intellij.execution.executors.DefaultRunExecutor;
-import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.runners.GenericProgramRunner;
-import com.intellij.execution.runners.RunContentBuilder;
-import com.intellij.execution.ui.RunContentDescriptor;
-import com.intellij.execution.ui.RunContentDescriptorReusePolicy;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.execution.ExecutionException
+import com.intellij.execution.configurations.RunProfile
+import com.intellij.execution.configurations.RunProfileState
+import com.intellij.execution.configurations.RunnerSettings
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.execution.runners.GenericProgramRunner
+import com.intellij.execution.runners.RunContentBuilder
+import com.intellij.execution.ui.RunContentDescriptor
+import com.intellij.execution.ui.RunContentDescriptorReusePolicy
 
-public class ElmTestProgramRunner extends GenericProgramRunner {
+class ElmTestProgramRunner : GenericProgramRunner<RunnerSettings>() {
 
-    @Nullable
-    @Override
-    protected RunContentDescriptor doExecute(RunProfileState state, ExecutionEnvironment environment) throws ExecutionException {
-        ExecutionResult result = state.execute(environment.getExecutor(), this);
-        RunContentDescriptor descriptor = new RunContentBuilder(result, environment).showRunContent(environment.getContentToReuse());
-        return withReusePolicy(descriptor);
+    @Throws(ExecutionException::class)
+    override fun doExecute(state: RunProfileState, environment: ExecutionEnvironment): RunContentDescriptor? {
+        val result = state.execute(environment.executor, this)
+        return RunContentBuilder(result!!, environment).showRunContent(environment.contentToReuse).makeReusable()
     }
 
-
-    @NotNull
-    private static RunContentDescriptor withReusePolicy(@NotNull RunContentDescriptor descriptor) {
-        descriptor.setReusePolicy(new RunContentDescriptorReusePolicy() {
-            @Override
-            public boolean canBeReusedBy(@NotNull RunContentDescriptor newDescriptor) {
-                return true;
-            }
-        });
-        return descriptor;
+    @Suppress("UnstableApiUsage")
+    private fun RunContentDescriptor.makeReusable(): RunContentDescriptor {
+        reusePolicy = object : RunContentDescriptorReusePolicy() {
+            override fun canBeReusedBy(newDescriptor: RunContentDescriptor) = true
+        }
+        return this
     }
 
-    @NotNull
-    @Override
-    public String getRunnerId() {
-        return "ELM_TEST_PROGRAM_RUNNER";
-    }
+    override fun getRunnerId() = "ELM_TEST_PROGRAM_RUNNER"
 
-    @Override
-    public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-        return DefaultRunExecutor.EXECUTOR_ID.equals(executorId) && profile instanceof ElmTestRunConfiguration;
-    }
+    override fun canRun(executorId: String, profile: RunProfile): Boolean =
+            DefaultRunExecutor.EXECUTOR_ID == executorId && profile is ElmTestRunConfiguration
 }

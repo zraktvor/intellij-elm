e901b860cf144f251d5e2d5d4e7f6e4d23735065
Keith Lazuka <kelazuka@microsoft.com>
2019-07-28 13:55:20 -0400

    convert ElmTestSettingsEditor

--- src/main/java/org/frawa/elmtest/run/ElmTestSettingsEditor.kt
+++ src/main/java/org/frawa/elmtest/run/ElmTestSettingsEditor.kt
@@ -11,50 +11,39 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package org.frawa.elmtest.run;
+package org.frawa.elmtest.run
 
-import com.intellij.openapi.options.SettingsEditor;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.ComboBox;
-import org.frawa.elmtest.core.ElmProjectTestsHelper;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.options.SettingsEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ComboBox
+import org.frawa.elmtest.core.ElmProjectTestsHelper
+import javax.swing.JComponent
+import javax.swing.JPanel
 
-import javax.swing.*;
+class ElmTestSettingsEditor(project: Project) : SettingsEditor<ElmTestRunConfiguration>() {
+    private val helper: ElmProjectTestsHelper = ElmProjectTestsHelper(project)
+    private lateinit var myPanel: JPanel
+    private lateinit var projectChooser: ComboBox<String>
 
-public class ElmTestSettingsEditor extends SettingsEditor<ElmTestRunConfiguration> {
-    private final ElmProjectTestsHelper helper;
-    private JPanel myPanel;
-    private ComboBox<String> projectChooser;
-
-    ElmTestSettingsEditor(Project project) {
-        helper = new ElmProjectTestsHelper(project);
-        helper
-                .allNames()
-                .forEach(projectChooser::addItem);
+    init {
+        helper.allNames().forEach { projectChooser.addItem(it) }
     }
 
-    @NotNull
-    @Override
-    protected JComponent createEditor() {
-        return myPanel;
+    override fun createEditor(): JComponent {
+        return myPanel
     }
 
-    @Override
-    protected void resetEditorFrom(@NotNull ElmTestRunConfiguration configuration) {
-        String dirPath = configuration.getOptions().getElmFolder();
-        if (dirPath == null) return;
-        String name = helper.nameByProjectDirPath(dirPath);
+    override fun resetEditorFrom(configuration: ElmTestRunConfiguration) {
+        val dirPath = configuration.options.elmFolder ?: return
+        val name = helper.nameByProjectDirPath(dirPath)
         if (name != null) {
-            projectChooser.setSelectedItem(name);
+            projectChooser.selectedItem = name
         }
     }
 
-    @Override
-    protected void applyEditorTo(@NotNull ElmTestRunConfiguration configuration) {
-        String name = (String) projectChooser.getSelectedItem();
-        if (name != null) {
-            String folder = helper.projectDirPathByName(name);
-            configuration.setOptions(new ElmTestRunConfiguration.Options(folder));
-        }
+    override fun applyEditorTo(configuration: ElmTestRunConfiguration) {
+        val name = projectChooser.selectedItem as String
+        val folder = helper.projectDirPathByName(name)
+        configuration.options = ElmTestRunConfiguration.Options(folder)
     }
 }
